6282a0cdca41aff78ac8ea1f55ccccd1b2d36dee
Fernando Bertos Matilla <fbertos@gmail.com>
2019-07-31 16:43:31 +0200

    Version 1.1

--- src/main/java/org/fbertos/spark/SparkDriverProgram.java
+++ src/main/java/org/fbertos/spark/SparkDriverProgram.java
@@ -8,32 +8,22 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.Optional;
-import org.apache.spark.api.java.function.PairFunction;
-import org.apache.spark.sql.SparkSession;
-
-import scala.Tuple2;
-
 
 public class SparkDriverProgram {
     @SuppressWarnings("unchecked")
 	public static void main(String args[]) throws Exception {
-    	 String driverName = "org.apache.hive.jdbc.HiveDriver";
+    	String driverName = "com.cloudera.impala.jdbc4.Driver";
     	 Class.forName(driverName);
 
-    	 Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000/default;hive.server2.proxy.user=APP", "APP", "mine");
+    	 Connection con = DriverManager.getConnection("jdbc:impala://localhost:21050/test", "", "");
     	 Statement stmt = con.createStatement();
     	 
     	 
     	 String sql = "select * from employee";
     	 ResultSet res = stmt.executeQuery(sql);
     	 
-    	 if (res.next()) {
-    	    System.out.println(res.getString(1));
+    	 while (res.next()) {
+    	    System.out.println(res.getString(1) + "\t" + res.getString(2));
     	 }
     }
 }
